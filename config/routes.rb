e684db5ad9690eeae0dc22f5221070c1012b44c2
Thang Pham <phamtoanthang@gmail.com>
2016-03-17 11:07:15 +0700

    LogInLogOut done - Remember Me to be done if have time

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'root'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  get 'signup'  => 'users#new'
   resources :users  
 end
