240f4817a417735a5752a1bd20eec579a14225c4
Thang Pham <phamtoanthang@gmail.com>
2016-03-19 23:20:55 +0700

    Correct bugs for send & receive messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317161524) do
+ActiveRecord::Schema.define(version: 20160319160104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20160317161524) do
 
   add_index "messages", ["recipent_id"], name: "index_messages_on_recipent_id", using: :btree
   add_index "messages", ["sender_id"], name: "index_messages_on_sender_id", using: :btree
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
